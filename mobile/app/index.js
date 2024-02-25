4f1f6179a51a3080acf3e3fc8a0ed8504ae1561d
Kareem <kareemhabtoor@gmail.com>
2024-02-25 03:09:12 +0300

    Mobile App

---
+++ mobile/app/index.js
@@ -0,0 +1,13 @@
+import { StatusBar, Text, View } from 'react-native';
+import React from 'react';
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default index;
